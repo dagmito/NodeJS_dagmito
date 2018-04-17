8af9d04c3db4fdb5489b8f8da438bef2fddbfe05
Dagmito <36495952+dagmito@users.noreply.github.com>
2018-04-16 21:04:04 -0300

    Commit Express npm Init

--- Primeira Aula de Node/ex2/app.js
+++ Primeira Aula de Node/ex2/app.js
@@ -5,7 +5,7 @@ var express = require('express');
 var server = http.createServer(function (request, response) {
     if (request.url === "/editar") {
         response.end(
-            '<html><head charset="utf-8" ></head><body><h1>Welcome to Bilola Master - Editar</h1></body></html>'
+            '<html><head charset="utf-8"></head><body><h1>Welcome to Bilola Master - Editar</h1></body></html>'
         );
     } else {
         response.end(
